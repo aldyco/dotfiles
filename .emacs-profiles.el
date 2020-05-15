d8dfe692fd6ae1515417348f4379ad9946d6ae4f
Aldyco Turang <aldycoturang@gmail.com>
2020-05-15 14:20:24 +0700

    Initial commit

---
+++ .emacs-profiles.el
@@ -0,0 +1,3 @@
+(("default" . ((user-emacs-directory . "~/.emacs.d")))
+ ("doom" . ((user-emacs-directory . "~/github/doom-emacs")
+            (env . (("DOOMDIR" . "~/.doom.d"))))))
---
+++ .lein/profiles.clj
@@ -0,0 +1,2 @@
+{:repl {:plugins [[cider/cider-nrepl "0.24.0"]
+                  [refactor-nrepl "2.5.0"]]}}
